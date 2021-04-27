d6e1a5189f22e0f1e89a4935def0e1aa479bba37
KOlegD <43730369+KOlegD@users.noreply.github.com>
2021-04-27 22:03:17 +0300

    Update web.py

--- web.py
+++ web.py
@@ -26,5 +26,5 @@ def index():
 
 if __name__ == '__main__':
     # app.run(port=8080, host='127.0.0.1')
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
